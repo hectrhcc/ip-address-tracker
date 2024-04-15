ef7c243500d219f43f455acb22ff86b9237747a5
Hector <hcontrerascorvacho@gmail.com>
2024-04-15 18:44:34 -0400

    interfaz2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export const Header = () => {
         </div>
         <div className='flex items-center justify-center pt-0'>
         <input className='text-lg cursor-pointer rounded-l-xl px-12 py-4 mb-5 bg-Almost-White text-Almost-Black lg:z-10' placeholder='Search for any IP address or domain'/>
-        <button className='cursor-pointer bg-Almost-Black rounded-r-xl text-Almost-White font-bold py-7.5 px-7 mb-5  bg-center bg-no-repeat lg:z-10' style={{  fontWeight: '800', backgroundImage: `url(${iconArrow})` }}></button>
+        <button className='cursor-pointer bg-Almost-Black rounded-r-xl  font-bold py-7.5 px-7 mb-5  bg-center bg-no-repeat lg:z-10' style={{  fontWeight: '800', backgroundImage: `url(${iconArrow})`, zIndex:10 }}></button>
         <div className="hidden lg:block absolute inset-0 bg-cover bg-center bg-no-repeat z-[0]" style={{ backgroundImage: `url(${patternBgDesktop})`,backgroundSize: 'auto', backgroundPosition: '50% 0%', zIndex:0}}></div>
         </div>
     </header>
