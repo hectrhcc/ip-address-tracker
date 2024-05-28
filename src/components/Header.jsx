3c85ae9e64130dbe220305255fb9937f12804af2
Hector <hcontrerascorvacho@gmail.com>
2024-05-28 18:56:21 -0400

    funcion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import patternBgMobile from '../../images/pattern-bg-mobile.png'; 
 import patternBgDesktop from '../../images/pattern-bg-desktop.png';
-import useFetch from "./useFetch";
 import { useContext } from 'react';
 import { IPContext } from './IPContext';
 import {useState, useEffect} from 'react'
@@ -9,9 +8,23 @@ import {useState, useEffect} from 'react'
 export const Header = () => {
  
   const { ip, setIp } = useContext(IPContext);
-  const {bandera, setBandera} = useState(0);
-  const [data] = useFetch(ip);//vacio luego se transforma en mi IP
-    
+  const [bandera, setBandera] = useState(0);
+  //uso la api para darle a el objeto ip todos los datos de mi ip
+  useEffect(() =>{
+    fetch(`https://ipwho.is/${ip}`)
+    .then((response) =>{
+      return response.json()
+    })
+    .then((data)=>{
+        setIp(data)/*data.ip ok pero habia que darle todos los datos 
+        ip es un objeto {ip} = Object Object
+        en cambio {ip.ip} = 179.x.x.x
+        */
+    })
+  },[] )
+  console.log('ip:',ip)
+  
+
   function handleChange(e) {
       setIp(e.target.value);//actualiza la ip
       }
@@ -21,11 +34,7 @@ function ipCompleta(){
 
 }
 //la ip por defecto a del usuario
-useEffect(() => {
-  if (data.ip) {
-    setIp(data.ip);
-  }
-}, [data.ip]);
+
 
 
   return (
@@ -36,7 +45,7 @@ useEffect(() => {
         <h1 className='font-rubik font-medium text-2xl mb-6 font-semibold pt-6 text-Almost-White lg:z-10'> IP Address Tracker </h1>
         </div>
         <div className='flex items-center justify-center pt-0'>
-        <input className='text-lg cursor-pointer rounded-l-xl px-12 py-4 mb-5 bg-Almost-White text-Almost-Black lg:z-10'  placeholder='Search for any IP address or domain' value={ip} onChange={handleChange}/>
+        <input className='text-lg cursor-pointer rounded-l-xl px-12 py-4 mb-5 bg-Almost-White text-Almost-Black lg:z-10'  placeholder='Search for any IP address or domain' value={ip.ip} onChange={handleChange}/>
         <button className='cursor-pointer bg-Almost-Black rounded-r-xl  font-bold py-7.5 px-7 mb-5  bg-center bg-no-repeat lg:z-10 bg-icon-arrow' style={{  fontWeight: '800'}} onClick={ipCompleta}></button>
         <div className="hidden lg:block absolute inset-0 bg-cover bg-center bg-no-repeat z-[0]" style={{ backgroundImage: `url(${patternBgDesktop})`,backgroundSize: 'auto', backgroundPosition: '50% 0%', zIndex:0}}></div>
         </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,76 @@
 import React from 'react'
-import useFetch from "./useFetch";
-
 import { useContext } from 'react';
 import { IPContext } from './IPContext';
 import {useState, useEffect} from 'react'
 
-
 export const Main = () => {
-
   const { ip } = useContext(IPContext);
-  const [data] = useFetch(ip);//data de la ip inicial
-  const [datos, setDatos] = useState([data]);  //data de las siguientes
-  
- 
-  
+  const [datos, setDatos] = useState({  
+    "ip": '',
+    "success":"",
+    "type": "",
+    "continent": "",
+    "continent_code": "",
+    "country": "",
+    "country_code": "",
+    "region": "",
+    "region_code": "",
+    "city": "",
+    "latitude":"",
+    "longitude":"",
+    "is_eu":"",
+    "postal": "",
+    "calling_code": "",
+    "capital": "",
+    "borders": "",
+    flag:{
+        "img": "",
+        "emoji": "",
+        "emoji_unicode": ""
+    },
+    connection:{
+        "asn": "",
+        "org": "",
+        "isp": "",
+        "domain": ""
+    },
+    timezone:{
+        "id": "",
+        "abbr": "",
+        "is_dst": "",
+        "offset": "",
+        "utc": "",
+        "current_time": ""
+    },
+    currency:{
+        "name": "",
+        "code": "",
+        "symbol": "",
+        "plural": "",
+        "exchange_rate": ""
+    },
+    security:{
+        "anonymous":"" ,
+        "proxy": "",
+        "vpn": "",
+        "tor": "",
+        "hosting":"" 
+    }
+})
+//uso la api para darle a el objeto datos todos los datos de mi ip
+  useEffect(() =>{
+    fetch(`https://ipwho.is/${ip}`)
+    .then((response) =>{
+      return response.json()
+    })
+    .then((dat)=>{
+        setDatos(dat)//JSON.stringify(dat)
+    })
+  },[] )
+  console.log('ip:',ip)
+  console.log('datos:',datos)
+
+
   useEffect(() => {
     fetch(`https://ipwho.is/${ip}`)
       .then((response) => {
@@ -23,23 +80,17 @@ export const Main = () => {
         setDatos(e)
       })
 }, [ip])
-//esto hay que entenderlo  bien y ver como unirlo con la funcion ipCompleta
-useEffect(() => {
-  if (data) {
-    setDatos(datos);
-  }
-}, [data]);
 
-console.log('ip main:', ip)
-console.log('datos:', datos) 
+
+
+console.log("datos", datos)
+//funciona con data. no con datos
   return (
   <main className='absolute inset-y-40 mx-4 h-72 lg:h-32 w-11/12  rounded-2xl bg-Almost-White lg:grid lg:grid-cols-4 lg:mx-14'>
-    <div className='flex flex-col justify-center items-center '><div className='font-rubik text-sm text-Medium-Gray font-bold tracking-widest mb-2 mt-8 lg:mt-0'>IP ADDRESS</div><div className='text-Almost-Black font-bold'> { data && data.ip}</div></div>
-    <div className='flex flex-col justify-center items-center'><div className='font-rubik text-sm  text-Medium-Gray font-bold  tracking-widest mb-2 mt-4 lg:mt-0'>LOCATION</div><div className='text-Almost-Black font-bold text-center'>{data  && data.city}, {data.region}, {data.country}</div></div>
-    <div className='flex flex-col justify-center items-center'><div className='font-rubik text-sm text-Medium-Gray font-bold  tracking-widest mb-2 mt-4 lg:mt-0'>TIMEZONE</div><div className='text-Almost-Black font-bold'>UTC {data && data.timezone.utc}</div></div>
-    <div className='flex flex-col justify-center items-center'><div className='font-rubik text-sm  text-Medium-Gray font-bold  tracking-widest mb-2 mt-4 lg:mt-0'>ISP</div><div className='text-Almost-Black font-bold text-center'>{data && data.connection.isp}</div></div>
-
+    <div className='flex flex-col justify-center items-center '><div className='font-rubik text-sm text-Medium-Gray font-bold tracking-widest mb-2 mt-8 lg:mt-0'>IP ADDRESS</div><div className='text-Almost-Black font-bold'> { datos && datos.ip}</div></div>
+    <div className='flex flex-col justify-center items-center'><div className='font-rubik text-sm  text-Medium-Gray font-bold  tracking-widest mb-2 mt-4 lg:mt-0'>LOCATION</div><div className='text-Almost-Black font-bold text-center'>{datos && datos.city}, {datos && datos.region}, {datos && datos.country}</div></div>
+    <div className='flex flex-col justify-center items-center'><div className='font-rubik text-sm text-Medium-Gray font-bold  tracking-widest mb-2 mt-4 lg:mt-0'>TIMEZONE</div><div className='text-Almost-Black font-bold'>UTC {datos.timezone && datos.timezone.utc}</div></div>
+    <div className='flex flex-col justify-center items-center'><div className='font-rubik text-sm  text-Medium-Gray font-bold  tracking-widest mb-2 mt-4 lg:mt-0'>ISP</div><div className='text-Almost-Black font-bold text-center'>{datos.connection && datos.connection.isp}</div></div>
 </main>
-
 )
 }
