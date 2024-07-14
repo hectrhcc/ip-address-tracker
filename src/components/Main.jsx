35b54d5edaf6ec2949d8ad75ccf88fca549ccccf
Hector <hcontrerascorvacho@gmail.com>
2024-07-14 18:29:53 -0400

    without popup4

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -82,7 +82,13 @@ console.log("coordenadas:", mapLocation)
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
-  const marker = L.marker([mapLocation.lat, mapLocation.lng]).addTo(map);
+  const customIcon = L.icon({
+    iconUrl: locationIcon,
+    iconSize: [32, 32], 
+    iconAnchor: [16, 32], 
+    popupAnchor: [0, -32] 
+  });
+  const marker = L.marker([mapLocation.lat, mapLocation.lng], { icon: customIcon }).addTo(map);
   /*const circle = L.circle([mapLocation.lat, mapLocation.lng], {
     color: 'red',
     fillColor: '#f03',
@@ -148,7 +154,6 @@ console.log("coordenadas:", mapLocation)
   <main className='absolute inset-y-40 mx-4 h-80 lg:h-32 w-11/12  rounded-2xl bg-Almost-White lg:grid lg:grid-cols-4 lg:mx-14 sm:pb-20 lg:pb-0'>
     <div className='flex flex-col justify-center items-center '><div className='font-rubik text-sm text-Medium-Gray font-bold tracking-widest mb-2 mt-8 lg:mt-0'>IP ADDRESS</div><div className='text-Almost-Black font-bold'> { datos && datos.ip}</div></div>
     <div className='flex flex-col justify-center items-center'><div className='font-rubik text-sm  text-Medium-Gray font-bold  tracking-widest mb-2 mt-4 lg:mt-0'>LOCATION</div><div className='text-Almost-Black font-bold text-center'>{datos && datos.city}, {datos && datos.region}, {datos && datos.country}
-    <img src={locationIcon} alt="Location Icon"  />
     </div></div>
     <div className='flex flex-col justify-center items-center'><div className='font-rubik text-sm text-Medium-Gray font-bold  tracking-widest mb-2 mt-4 lg:mt-0'>TIMEZONE</div><div className='text-Almost-Black font-bold'>UTC {datos.timezone && datos.timezone.utc}</div></div>
     <div className='flex flex-col justify-center items-center'><div className='font-rubik text-sm  text-Medium-Gray font-bold  tracking-widest mb-2 mt-4 lg:mt-0'>ISP</div><div className='text-Almost-Black font-bold text-center'>{datos.connection && datos.connection.isp}</div></div>
